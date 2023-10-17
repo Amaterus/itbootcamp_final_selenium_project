76971fce84b36f3892ae8af62fcacbb0d8eb608e
Veljko Andjelkovic <veljkoandjelkovic2000@gmail.com>
2023-10-17 15:45:34 +0200

    Verify error message when user already exists

--- src/test/java/tests/SignupTests.java
+++ src/test/java/tests/SignupTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -23,5 +25,25 @@ public class SignupTests extends BasicTest{
         signupPage.checkEmailInputType();
         signupPage.checkPasswordInputType();
         signupPage.checkConfirmPassowordInputType();
+    }
+    @Test(priority = 3, retryAnalyzer = RetryAnalyzer.class)
+    public void displaysErrorsWhenUserAlreadyExists() throws InterruptedException {
+        navPage.clickOnSignUpButton();
+
+        wait
+                .until(ExpectedConditions.titleContains("Signup - My Awesome App"));
+
+        signupPage.fillSignUpFormWithData("Another User", "admin@admin.com", "12345", "12345");
+
+        signupPage.clickOnSignUpButton();
+
+        wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div > div.v-snack__content")));
+
+
+        WebElement errorMessage = driver.findElement(By.cssSelector("div > div.v-snack__content > ul > li"));
+        Assert.assertTrue(errorMessage.getText().contains("E-mail already exists"));
+
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl + "/signup");
     }
 }
